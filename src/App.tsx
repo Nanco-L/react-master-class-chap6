dce661a92d2eef021d9c1043445a75af7d3c6ab0
Kyuhyun Lee <khlee1992@naver.com>
2023-04-13 08:17:32 +0900

    Until lecture #7.5

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,82 @@
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import {
+  DragDropContext,
+  Draggable,
+  DropResult,
+  Droppable,
+} from "react-beautiful-dnd";
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
+import { toDoState } from "./atoms";
 
 const Wrapper = styled.div`
-    display: flex;
-    max-width: 480px;
-    width: 100%;
-    margin: 0 auto;
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr);
 `;
 
 const Board = styled.div`
-    background-color: ${(props) => props.theme.boardColor};
+  padding: 20px 10px;
+  padding-top: 30px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
 `;
 
 const Card = styled.div`
-    background-color: ${(props) => props.theme.cardColor};
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 10px 10px;
+  background-color: ${(props) => props.theme.cardColor};
 `;
 
 function App() {
-    const onDragEnd = () => {};
-    return (
-        <DragDropContext onDragEnd={onDragEnd}>
-            <Wrapper>
-                <Droppable droppableId="one">
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const onDragEnd = ({ destination, source }: DropResult) => {
+    setToDos((value) => {
+      const draggedItem = value.splice(source.index, 1);
+      //   return toDos.splice(destination.index, 0, draggedItem);
+      return value;
+    });
+    console.log(toDos);
+  };
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Wrapper>
+        <Boards>
+          <Droppable droppableId="one">
+            {(magic) => (
+              <Board ref={magic.innerRef} {...magic.droppableProps}>
+                {toDos.map((toDo, index) => (
+                  <Draggable key={index} draggableId={toDo} index={index}>
                     {(magic) => (
-                        <Board ref={magic.innerRef} {...magic.droppableProps}>
-                            <Draggable draggableId="first" index={0}>
-                                {(magic) => (
-                                    <Card
-                                        ref={magic.innerRef}
-                                        {...magic.draggableProps}
-                                        {...magic.dragHandleProps}
-                                    >
-                                        Hello
-                                    </Card>
-                                )}
-                            </Draggable>
-                            <Draggable draggableId="second" index={1}>
-                                {(magic) => (
-                                    <Card
-                                        ref={magic.innerRef}
-                                        {...magic.dragHandleProps}
-                                        {...magic.draggableProps}
-                                    >
-                                        Hello!
-                                    </Card>
-                                )}
-                            </Draggable>
-                        </Board>
+                      <Card
+                        ref={magic.innerRef}
+                        {...magic.draggableProps}
+                        {...magic.dragHandleProps}
+                      >
+                        {toDo}
+                      </Card>
                     )}
-                </Droppable>
-            </Wrapper>
-        </DragDropContext>
-    );
+                  </Draggable>
+                ))}
+                {magic.placeholder}
+              </Board>
+            )}
+          </Droppable>
+        </Boards>
+      </Wrapper>
+    </DragDropContext>
+  );
 }
 
 export default App;
